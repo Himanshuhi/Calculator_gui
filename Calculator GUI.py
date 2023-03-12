69af4121659c744be39f98a1a08173e2a968799d
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-12 19:11:10 +0530

    Update Calculator GUI.py

--- Calculator GUI.py
+++ Calculator GUI.py
@@ -5,11 +5,11 @@ class Calculator:
         self.master = master
         master.title("Calculator")
         
-        # Entry widget to display the result
+        # himanshu
         self.result = tk.Entry(master, width=24, font=('Arial', 16))
         self.result.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
         
-        # List of calculator buttons and their positions
+      
         buttons = [
             {'text': '7', 'row': 1, 'col': 0},
             {'text': '8', 'row': 1, 'col': 1},
@@ -29,7 +29,7 @@ class Calculator:
             {'text': '+', 'row': 4, 'col': 3},
         ]
         
-        # Create and place the calculator buttons
+       
         for button in buttons:
             text = button['text']
             row = button['row']
@@ -37,12 +37,12 @@ class Calculator:
             cmd = lambda text=text: self.button_click(text)
             tk.Button(master, text=text, width=6, height=2, font=('Arial', 16), command=cmd).grid(row=row, column=col, padx=5, pady=5)
         
-        # Clear button
+       
         tk.Button(master, text='C', width=6, height=2, font=('Arial', 16), command=self.clear_result).grid(row=5, column=0, padx=5, pady=5)
         
     def button_click(self, text):
         if text == '=':
-            # Evaluate the expression and update the result
+            
             try:
                 result = str(eval(self.result.get()))
             except:
@@ -50,7 +50,7 @@ class Calculator:
             self.result.delete(0, tk.END)
             self.result.insert(0, result)
         else:
-            # Add the clicked button text to the expression
+           
             self.result.insert(tk.END, text)
         
     def clear_result(self):
